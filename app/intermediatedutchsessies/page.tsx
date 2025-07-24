30c7dc1429b35d901e1b4f68ea9a45f3d49adb87
Michael Bernard <mike_bernard@live.nl>
2025-07-24 06:39:49 +0200

    add course intermediatedutchsessies

---
+++ app/intermediatedutchsessies/page.tsx
@@ -0,0 +1,10 @@
+import IntermediateDutchClient from "./client-component";
+
+export const metadata = {
+  title: "Intermediate Nederlands (A2–B1)",
+  description: "Overzicht van de cursus Intermediate Nederlands. Versterk je taalvaardigheden met uitdagende sessies en huiswerkopdrachten.",
+};
+
+export default function Page() {
+  return <IntermediateDutchClient />;
+}
