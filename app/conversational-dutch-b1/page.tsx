9bc0c1e62c63c26f27a20f6a94de37f818ec8821
Michael Bernard <mike_bernard@live.nl>
2025-09-30 05:08:58 +0200

    add conversational B1

---
+++ app/conversational-dutch-b1/page.tsx
@@ -0,0 +1,10 @@
+import ConversationalDutchClientB1 from "./client-component";
+
+export const metadata = {
+  title: "Conversational Dutch - B1",
+  description: "Overzicht van de cursus Conversational Dutch - B1. Leer vlot Nederlands praten in verschillende thema's.",
+};
+
+export default function Page() {
+  return <ConversationalDutchClientB1 />;
+}
