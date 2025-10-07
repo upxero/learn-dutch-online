8d300f2a0bd83a06119212256c05b94b60569450
Michael Bernard <mike_bernard@live.nl>
2025-10-07 06:46:21 +0200

    change corporate

--- app/corporate/layout.tsx
+++ app/corporate/layout.tsx
@@ -1,34 +1,7 @@
-"use client";
-
-import { useEffect } from "react";
-
-import AOS from "aos";
-import "aos/dist/aos.css";
-
-import Header from "@/components/ui/header";
-import Footer from "@/components/ui/footer";
-
-export default function DefaultLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  useEffect(() => {
-    AOS.init({
-      once: true,
-      disable: "phone",
-      duration: 700,
-      easing: "ease-out-cubic",
-    });
-  });
-
+export default function CorporateLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <Header />
-
-      <main className="grow">{children}</main>
-
-      <Footer border={true} />
-    </>
+    <div className="font-sans text-gray-800 max-w-6xl mx-auto p-6">
+      {children}
+    </div>
   );
 }
