31347aaa22038754b04853ff333e586085a2572b
Michael Bernard <mike_bernard@live.nl>
2025-11-11 04:11:33 +0100

    add facebook and twitter pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
-        {/* ✅ Google Tag Manager script */}
+        {/* ✅ Google Tag Manager */}
         <Script id="gtm-script" strategy="afterInteractive">
           {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -32,6 +32,39 @@ export default function RootLayout({
             })(window,document,'script','dataLayer','GTM-TQM7THNV');
           `}
         </Script>
+
+        {/* ✅ Facebook Meta Pixel */}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '664468612398627');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        {/* ✅ Twitter Pixel */}
+        <Script id="twitter-pixel" strategy="afterInteractive">
+          {`
+            !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+            },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+            twq('config','o8wgp');
+          `}
+        </Script>
+
+        {/* ✅ Placeholder Reddit Pixel (voeg code later toe) */}
+        {/* <Script id="reddit-pixel" strategy="afterInteractive">
+          {`
+            // Reddit Pixel code hier
+          `}
+        </Script> */}
       </head>
 
       <body
@@ -45,6 +78,17 @@ export default function RootLayout({
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
+        </noscript>
+
+        {/* ✅ Facebook Pixel (noscript fallback) */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=664468612398627&ev=PageView&noscript=1"
+            alt="fb-pixel-noscript"
+          />
         </noscript>
 
         <div className="flex min-h-screen flex-col overflow-hidden supports-[overflow:clip]:overflow-clip">
